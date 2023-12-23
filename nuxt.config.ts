fb791694a186f7eab398fe96f7647ea71a42188f
Jesús Aurelio Castro Magaña <jesus_castrom@my.unitec.edu.mx>
2023-12-23 17:48:46 -0600

    Mini Update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,25 @@
 export default defineNuxtConfig({
-  // https://github.com/nuxt-themes/docus
-  extends: '@nuxt-themes/docus',
-  css: [
-    '~/assets/css/prose-code.css'
-  ],
-  devtools: { enabled: true },
-  modules: ['@nuxt/content'],
-  content: {
-    highlight: {
-      theme: 'dark-plus',
-      preload: [
-        'c',
-        'cpp',
-        'java'
-      ]
+    // https://github.com/nuxt-themes/docus
+    extends: '@nuxt-themes/docus',
+    css: [
+        '~/assets/css/prose-code.css'
+    ],
+    devtools: {enabled: true},
+    modules: ['@nuxt/content'],
+    content: {
+        highlight: {
+            theme: 'dark-plus',
+            preload: [
+                'c',
+                'cpp',
+                'java',
+                'sql',
+                'ts',
+                'json',
+                'kotlin',
+                'css',
+                'php'
+            ]
+        },
     },
-  },
 })
