862993314339c6a5ff46b27a4ae2728029a0abe5
Jesús Aurelio Castro Magaña <jesus_castrom@my.unitec.edu.mx>
2023-12-24 03:03:40 -0600

    Mini Update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,13 @@
 export default defineNuxtConfig({
     extends: '@nuxt-themes/docus',
+    components: [
+        '~/node_modules/element-plus/**/*.vue'
+    ],
     css: [
         '~/assets/css/prose-code.css'
     ],
     devtools: {enabled: true},
-    modules: ['@nuxt/content', '@element-plus/nuxt'],
+    modules: ['@nuxt/components', '@nuxt/content', '@element-plus/nuxt'],
     content: {
         highlight: {
             theme: 'dark-plus',
